a923a348b045db7512ade03acc77ee54ccc8c559
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-10-30 21:41:30 -0300

    transcribing audio from .wav to text

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
 import pyaudio
 import wave
+import speech_recognition as sr
+import os
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -9,6 +11,7 @@ RECORD_SECONDS = 5
 OUTPUT_FILENAME = 'audio.wav'
 
 audio = pyaudio.PyAudio()
+recognizer = sr.Recognizer()
 
 stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
@@ -33,3 +36,19 @@ with wave.open(OUTPUT_FILENAME, 'wb') as wf:
    wf.writeframes(b''.join(frames))
 
 print(f"Save file with name {OUTPUT_FILENAME}")
+
+with sr.AudioFile("audio.wav") as source:
+   print("Processando o áudio...")
+   audio_data = recognizer.record(source)
+
+try:
+   text = recognizer.recognize_google(audio_data, language="pt-BR") 
+   print("Texto transcrito:")
+   print(text)
+except sr.UnknownValueError:
+   print("O áudio não pôde ser reconhecido.")
+except sr.RequestError as e:
+   print(f"Erro ao se conectar ao serviço de reconhecimento: {e}")
+
+if os.path.exists('audio.wav'):
+   os.remove('audio.wav')
