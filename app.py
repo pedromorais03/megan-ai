aa9771a82c7ca917a5eb29a33753702f11fdbea7
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-12-28 20:04:49 -0300

    created upload route and error template page

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+   os.makedirs(UPLOAD_FOLDER)
+
+# Configuring flask to receive 10MB files
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 # 10MB
 
 @app.route('/')
 def home():
    return render_template("home.html")
 
+@app.route('/record')
+def record():
+   return render_template("generate.html")
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+   if 'audio' not in request.files:
+      # return jsonify({"error": "No audio file part"}), 400
+      return render_template("error.html")
+   
+   audio_file = request.files['audio']
+   if audio_file.filename == '':
+      #  return jsonify({"error": "No selected file"}), 400
+      return render_template("error.html")
+   
+   file_path = os.path.join(UPLOAD_FOLDER, audio_file.filename)
+   audio_file.save(file_path)
+
+   return jsonify({"message:" f"Audio saved successfully. {file_path}"}), 200
+
 @app.route('/generate')
 def generate():
-   return render_template("generate.html")
+   print("Generating...")
 
 if  __name__ == '__main__':
    app.run(debug=True)
